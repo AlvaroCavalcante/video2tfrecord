a164ea020cc913061e35b9293f96ca1c6867cf0b
Alvaro Carneiro <leandro0807@live.com>
2022-03-21 08:05:11 -0300

    correct identation based on pep8

--- hand_face_detection.py
+++ hand_face_detection.py
@@ -14,29 +14,34 @@ try:
 except:
     print('GPU not found')
 
+
 def get_centroids(bouding_boxes):
     centroids = []
-    
+
     for object in bouding_boxes:
         class_name = list(object.keys())[0]
         bbox = object.get(class_name)
         xmin, xmax, ymin, ymax = bbox['xmin'], bbox['xmax'], bbox['ymin'], bbox['ymax']
         centroid = (int((xmin+xmax)/2), int((ymin+ymax)/2))
         centroids.append(centroid)
-    
+
     return centroids
+
 
 def get_angle(opposite, adjacent_1, adjacent_2):
     # lei dos cossenos: https://pt.khanacademy.org/math/trigonometry/trig-with-general-triangles/law-of-cosines/v/law-of-cosines-missing-angle
-    cos_value = ((adjacent_1**2 + adjacent_2**2) - opposite**2) / (2*(adjacent_1*adjacent_2))
+    cos_value = ((adjacent_1**2 + adjacent_2**2) -
+                 opposite**2) / (2*(adjacent_1*adjacent_2))
     rad = math.acos(cos_value)
 
-    degrees = rad * 180 / math.pi 
+    degrees = rad * 180 / math.pi
 
     return degrees
 
+
 def compute_triangle_features(centroids, img, draw_on_img):
-    euclidian_distances, img = compute_centroids_distances(img, draw_on_img, centroids)
+    euclidian_distances, img = compute_centroids_distances(
+        img, draw_on_img, centroids)
 
     d1, d2, d3 = euclidian_distances[0], euclidian_distances[1], euclidian_distances[2]
     triangle_features = {}
@@ -48,27 +53,34 @@ def compute_triangle_features(centroids, img, draw_on_img):
     triangle_features['semi_perimeter'] = triangle_features['perimeter'] / 2
     triangle_features['area'] = math.sqrt(   # Fórmula de Heron https://www.todamateria.com.br/area-do-triangulo/
         (triangle_features['semi_perimeter'] * (triangle_features['semi_perimeter'] - d1) * (
-        triangle_features['semi_perimeter'] - d2) * (triangle_features['semi_perimeter'] - d3)))
+            triangle_features['semi_perimeter'] - d2) * (triangle_features['semi_perimeter'] - d3)))
 
-    triangle_features['ang_inter_a'] = get_angle(d3, d1, d2) 
+    triangle_features['ang_inter_a'] = get_angle(d3, d1, d2)
     triangle_features['ang_inter_b'] = get_angle(d1, d2, d3)
-    triangle_features['ang_inter_c'] = 180.0 - (triangle_features['ang_inter_a'] + triangle_features['ang_inter_b'])
+    triangle_features['ang_inter_c'] = 180.0 - \
+        (triangle_features['ang_inter_a'] + triangle_features['ang_inter_b'])
 
     # teorema dos Ângulos externos https://pt.wikipedia.org/wiki/Teorema_dos_%C3%A2ngulos_externos
-    triangle_features['ang_ext_a'] = triangle_features['ang_inter_b'] + triangle_features['ang_inter_c']
-    triangle_features['ang_ext_b'] = triangle_features['ang_inter_a'] + triangle_features['ang_inter_c']
-    triangle_features['ang_ext_c'] = triangle_features['ang_inter_b'] + triangle_features['ang_inter_a']
+    triangle_features['ang_ext_a'] = triangle_features['ang_inter_b'] + \
+        triangle_features['ang_inter_c']
+    triangle_features['ang_ext_b'] = triangle_features['ang_inter_a'] + \
+        triangle_features['ang_inter_c']
+    triangle_features['ang_ext_c'] = triangle_features['ang_inter_b'] + \
+        triangle_features['ang_inter_a']
 
     return triangle_features, img
+
 
 def compute_features_and_draw_lines(bouding_boxes, img, draw_on_img=False):
     centroids = get_centroids(bouding_boxes)
 
     triangle_features = {}
     if len(centroids) == 3:
-        triangle_features, img = compute_triangle_features(centroids, img, draw_on_img)
+        triangle_features, img = compute_triangle_features(
+            centroids, img, draw_on_img)
 
     return img, triangle_features
+
 
 def compute_centroids_distances(img, draw_on_img, centroids):
     euclidian_distances = []
@@ -76,15 +88,18 @@ def compute_centroids_distances(img, draw_on_img, centroids):
         centroid_1 = centroid_comb[0]
         centroid_2 = centroid_comb[1]
         if draw_on_img:
-            cv2.line(img, (centroid_1[0], centroid_1[1]), (centroid_2[0], centroid_2[1]), (0, 255, 0), thickness=5)
-        distance = math.sqrt((centroid_1[0]-centroid_2[0])**2+(centroid_1[1]-centroid_2[1])**2)
+            cv2.line(img, (centroid_1[0], centroid_1[1]),
+                     (centroid_2[0], centroid_2[1]), (0, 255, 0), thickness=5)
+        distance = math.sqrt(
+            (centroid_1[0]-centroid_2[0])**2+(centroid_1[1]-centroid_2[1])**2)
         euclidian_distances.append(distance)
 
     return euclidian_distances, img
 
+
 def filter_boxes_and_draw(image_np_with_detections, label_map_path, scores, classes, boxes, heigth, width, draw_on_image=False):
     category_index = label_map_util.create_category_index_from_labelmap(label_map_path,
-                                                            use_display_name=True)
+                                                                        use_display_name=True)
 
     output_bboxes = []
     hand_counter = 2
@@ -93,21 +108,25 @@ def filter_boxes_and_draw(image_np_with_detections, label_map_path, scores, clas
         class_name = category_index[classes[i]].get('name')
 
         if face_counter == 0 and hand_counter == 0:
-            return image_np_with_detections, output_bboxes      
+            return image_np_with_detections, output_bboxes
         elif class_name == 'face' and face_counter == 0:
             continue
         elif class_name == 'hand' and hand_counter == 0:
             continue
 
-        class_name = 'hand_'+str(hand_counter) if class_name == 'hand' else class_name
-        
-        xmin, xmax, ymin, ymax = boxes[i][1], boxes[i][3], boxes[i][0], boxes[i][2]
-        xmin, xmax, ymin, ymax = int(xmin * width), int(xmax * width), int(ymin * heigth), int(ymax * heigth)
-        output_bboxes.append({class_name: {'xmin': xmin, 'xmax': xmax, 'ymin': ymin, 'ymax': ymax}})
+        class_name = 'hand_' + \
+            str(hand_counter) if class_name == 'hand' else class_name
 
-        if draw_on_image:        
-            color = (0,255,0) if class_name == 'face' else (255,0,0)
-            cv2.rectangle(image_np_with_detections, (xmin, ymin), (xmax, ymax), color, 2)
+        xmin, xmax, ymin, ymax = boxes[i][1], boxes[i][3], boxes[i][0], boxes[i][2]
+        xmin, xmax, ymin, ymax = int(
+            xmin * width), int(xmax * width), int(ymin * heigth), int(ymax * heigth)
+        output_bboxes.append(
+            {class_name: {'xmin': xmin, 'xmax': xmax, 'ymin': ymin, 'ymax': ymax}})
+
+        if draw_on_image:
+            color = (0, 255, 0) if class_name == 'face' else (255, 0, 0)
+            cv2.rectangle(image_np_with_detections,
+                          (xmin, ymin), (xmax, ymax), color, 2)
 
         if class_name == 'face':
             face_counter -= 1
@@ -126,8 +145,9 @@ def get_image_segments(input_image, bouding_boxes):
         class_name = list(object.keys())[0]
         bbox = object.get(class_name)
 
-        img_segment = input_image[bbox['ymin']:bbox['ymax'], bbox['xmin']:bbox['xmax']]
-        
+        img_segment = input_image[bbox['ymin']
+            :bbox['ymax'], bbox['xmin']:bbox['xmax']]
+
         if class_name == 'face':
             face = img_segment
         elif class_name == 'hand_1':
@@ -136,6 +156,7 @@ def get_image_segments(input_image, bouding_boxes):
             hand_2 = img_segment
 
     return face, hand_1, hand_2
+
 
 def infer_images(image, label_map_path, detect_fn, heigth, width):
     image_np = np.array(image)
@@ -146,26 +167,31 @@ def infer_images(image, label_map_path, detect_fn, heigth, width):
 
     num_detections = int(detections.pop('num_detections'))
     detections = {key: value[0, :num_detections].numpy()
-                for key, value in detections.items()}
+                  for key, value in detections.items()}
     detections['num_detections'] = num_detections
 
-    detections['detection_classes'] = detections['detection_classes'].astype(np.int64)
+    detections['detection_classes'] = detections['detection_classes'].astype(
+        np.int64)
 
-    image_np_with_detections, bouding_boxes = filter_boxes_and_draw(
-        image.copy(), 
-        label_map_path, 
-        detections['detection_scores'], 
-        detections['detection_classes'], 
+    image_np_with_detections, bouimg, distances, triangle_featuresding_boxes = filter_boxes_and_draw(
+        image.copy(),
+        label_map_path,
+        detections['detection_scores'],
+        detections['detection_classes'],
         detections['detection_boxes'],
         heigth, width)
 
     return image_np_with_detections, bouding_boxes
 
+
 def detect_visual_cues_from_image(**kwargs):
     input_image = kwargs.get('image')
-    drawn_image, bouding_boxes = infer_images(input_image, kwargs.get('label_map_path'), kwargs.get('detect_fn'), kwargs.get('height'), kwargs.get('width'))
-    drawn_image, triangle_features = compute_features_and_draw_lines(bouding_boxes, drawn_image)
+    drawn_image, bouding_boxes = infer_images(input_image, kwargs.get(
+        'label_map_path'), kwargs.get('detect_fn'), kwargs.get('height'), kwargs.get('width'))
+    drawn_image, triangle_features = compute_features_and_draw_lines(
+        bouding_boxes, drawn_image)
 
-    face_segment, hand_1, hand_2 = get_image_segments(input_image, bouding_boxes)
-    
+    face_segment, hand_1, hand_2 = get_image_segments(
+        input_image, bouding_boxes)
+
     return face_segment, hand_1, hand_2, triangle_features
