14b1def53712b7b44c3f563606840d10edf1815c
Alvaro Carneiro <leandro0807@live.com>
2022-03-31 23:56:42 -0300

    fixing algorithm to read dataset

--- read_dataset.py
+++ read_dataset.py
@@ -3,36 +3,51 @@ import matplotlib.pyplot as plt
 import numpy as np 
 
 def read_tfrecord(example_proto):   
-    image_seq = []
+    face = []
+    hand_1 = []
+    hand_2 = []
 
-    for image_count in range(24-8):
-        path = 'blob' + '/' + str(image_count)
+    for image_count in range(24):
+        face_stream = 'face' + '/' + str(image_count)
+        hand_1_stream = 'hand_1' + '/' + str(image_count)
+        hand_2_stream = 'hand_2' + '/' + str(image_count)
 
-        feature_dict = {path: tf.io.FixedLenFeature([], tf.string),
-        'height': tf.io.FixedLenFeature([], tf.int64),
-        'width': tf.io.FixedLenFeature([], tf.int64),
-        'depth': tf.io.FixedLenFeature([], tf.int64),
-        'label': tf.io.FixedLenFeature([], tf.int64)}
+        feature_dict = {
+            face_stream: tf.io.FixedLenFeature([], tf.string),
+            hand_1_stream: tf.io.FixedLenFeature([], tf.string),
+            hand_2_stream: tf.io.FixedLenFeature([], tf.string),
+            'height': tf.io.FixedLenFeature([], tf.int64),
+            'width': tf.io.FixedLenFeature([], tf.int64),
+            'depth': tf.io.FixedLenFeature([], tf.int64),
+            'label': tf.io.FixedLenFeature([], tf.int64)
+        }
 
         features = tf.io.parse_single_example(example_proto, features=feature_dict)
 
         # width = tf.cast(features['width'], tf.int32)
         # height = tf.cast(features['height'], tf.int32)
-        width = 240 
-        height = 240
+        width = 50 
+        height = 50
 
-        image = tf.image.decode_jpeg(features[path], channels=3)
-        image = tf.image.resize(image, [width, height])
-        image = tf.reshape(image, tf.stack([height, width, 3]))
-        image = tf.reshape(image, [1, height, width, 3])
-        image = tf.cast(image, dtype='uint8')
-        image_seq.append(image)
+        face_image = get_image(features[face_stream], width, height)
+        hand_1_image = get_image(features[hand_1_stream], width, height)
+        hand_2_image = get_image(features[hand_2_stream], width, height)
+        
+        face.append(face_image)
+        hand_1.append(hand_1_image)
+        hand_2.append(hand_2_image)
 
         label = tf.cast(features['label'], tf.int32)
 
-    image_seq = tf.concat(image_seq, 0)
+    return [face, hand_1, hand_2], label
 
-    return image_seq, label
+def get_image(img, width, height):
+    image = tf.image.decode_jpeg(img, channels=3)
+    image = tf.image.resize(image, [width, height])
+    # image = tf.reshape(image, tf.stack([height, width, 3]))
+    # image = tf.reshape(image, [1, height, width, 3])
+    image = tf.cast(image, dtype='uint8')
+    return image
 
 def load_dataset(tf_record_path):
     raw_dataset = tf.data.TFRecordDataset(tf_record_path)
@@ -57,18 +72,18 @@ def load_data_tfrecord(tfrecord_path):
   dataset = prepare_for_training(dataset)
   return dataset
 
-tf_record_path = tf.io.gfile.glob('/home/alvaro/Documentos/video2tfrecord/example/output/*.tfrecords')
-
+tf_record_path = tf.io.gfile.glob('/home/alvaro/Área de Trabalho/video2tfrecord/example/train/*.tfrecords')
 row = 4; col = 4
 #row = min(row,15//col)
 
 all_elements = load_data_tfrecord(tf_record_path).unbatch()
 augmented_element = all_elements.repeat().batch(1)
 
+
 for (seq, label) in augmented_element:
     plt.figure(figsize=(15,int(15*row/col)))
     for j in range(row*col):
         plt.subplot(row,col,j+1)
         plt.axis('off')
-        plt.imshow(np.array(seq[0, j,]))
+        plt.imshow(np.array(seq[1, 2, j,]))
     plt.show()
